1a8e8a98d790a9e00f2d64cca3897d1578a9cd2f
NethmiS <20688651@student.curtin.edu.au>
2023-07-02 19:21:27 +0530

    Pharmacy update 2 with interfaces and firebase

--- app/src/main/java/com/example/capstoneprojectgroup4/PharmaciesF.java
+++ app/src/main/java/com/example/capstoneprojectgroup4/PharmaciesF.java
@@ -1,18 +1,28 @@
 package com.example.capstoneprojectgroup4;
 
+import android.app.SearchManager;
+import android.app.appsearch.SearchResults;
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
-import com.google.firebase.database.DatabaseReference;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
 
 /**
@@ -30,9 +40,18 @@ public class PharmaciesF extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    Button databaseB;
-    EditText sentenceT;
-    FirebaseDatabase database;
+
+
+    Toolbar toolbar;
+
+    Button orderButton;
+
+
+    RecyclerView recyclerView;
+    PharmacyAdapter pharmacyAdapter;
+    private SearchView searchView;
+    FragmentManager fm;
+
 
     public PharmaciesF() {
         // Required empty public constructor
@@ -58,6 +77,8 @@ public class PharmaciesF extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
@@ -71,28 +92,95 @@ public class PharmaciesF extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_pharmacies, container, false);
 
-        databaseB = v.findViewById(R.id.database_b);
-        sentenceT = v.findViewById(R.id.sentence_t);
 
-        database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
+        toolbar = v.findViewById(R.id.toolbar);
+        orderButton = v.findViewById(R.id.orderB);
 
-        databaseB.setOnClickListener(new View.OnClickListener() {
+        orderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String sentence = sentenceT.getText().toString();
-
-                if(TextUtils.isEmpty(sentence)){
-                    Toast.makeText(getActivity(), "Text field cannot be empty.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                myRef.setValue(sentence);
+                fm = getActivity().getSupportFragmentManager();
+                Delivery_Track welcomeF = new Delivery_Track();
+               fm.beginTransaction().replace(R.id.fragment_container, welcomeF).commit();
 
             }
         });
 
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setTitle("Search Pharmacies");
+
+
+        recyclerView = v.findViewById(R.id.recyclerviewId);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        FirebaseRecyclerOptions<Pharmacy> options =
+                new FirebaseRecyclerOptions.Builder<Pharmacy>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Pharmacies"), Pharmacy.class)
+                        .build();
+
+        pharmacyAdapter = new PharmacyAdapter(options);
+        recyclerView.setAdapter(pharmacyAdapter);
+
+
+
         return v;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        pharmacyAdapter.startListening();;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        pharmacyAdapter.stopListening();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.search_pharma, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+        searchView = (SearchView) menuItem.getActionView();
+        searchView.setIconified(true);
+
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                textSearch(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                textSearch(s);
+                return false;
+            }
+        });
+
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+
+
+     public void textSearch(String str)
+     {
+         FirebaseRecyclerOptions<Pharmacy> options =
+                 new FirebaseRecyclerOptions.Builder<Pharmacy>()
+                         .setQuery(FirebaseDatabase.getInstance().getReference().child("Pharmacies").orderByChild("Name").startAt(str).endAt(str+ "~"), Pharmacy.class)
+                         .build();
+
+         pharmacyAdapter = new PharmacyAdapter(options);
+         pharmacyAdapter.startListening();
+         recyclerView.setAdapter(pharmacyAdapter);
+     }
 }
