33c6c35354e5ce64aa6a9b5f90ed9d11d3d70f90
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2023-08-06 22:33:42 +0530

    Created a New Empty Activity called "Manage_Patient_Record_Activity"

---
+++ app/src/main/java/com/example/capstoneprojectgroup4/Manage_Patient_Record_Activity.java
@@ -0,0 +1,16 @@
+package com.example.capstoneprojectgroup4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Manage_Patient_Record_Activity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_patient_record);
+    }
+}
