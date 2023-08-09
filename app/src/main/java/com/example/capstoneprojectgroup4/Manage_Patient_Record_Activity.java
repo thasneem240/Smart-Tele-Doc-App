a4634569ad8b2f95cd4d59e563834dbf6514c4c8
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2023-08-09 17:53:21 +0530

    Intialized and Grabbed All the Variables from Layout file

--- app/src/main/java/com/example/capstoneprojectgroup4/Manage_Patient_Record_Activity.java
+++ app/src/main/java/com/example/capstoneprojectgroup4/Manage_Patient_Record_Activity.java
@@ -3,14 +3,31 @@ package com.example.capstoneprojectgroup4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Manage_Patient_Record_Activity extends AppCompatActivity
 {
+    /* Initialize Variables */
+
+    private Button button_PatientDetails;
+    private Button button_MedicalHistory;
+    private Button button_LabReports;
+    private Button button_ConsNotes;
+    private Button button_Prescriptions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_patient_record);
+
+        /* Grab the  Variables from Layout file*/
+
+        button_PatientDetails = findViewById(R.id.button_PatientDetails);
+        button_MedicalHistory = findViewById(R.id.button_MedicalHistory);
+        button_LabReports = findViewById(R.id.button_LabReports);
+        button_ConsNotes = findViewById(R.id.button_ConsNotes);
+        button_Prescriptions = findViewById(R.id.button_Prescriptions);
     }
 }
