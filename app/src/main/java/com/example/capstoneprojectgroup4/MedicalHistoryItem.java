1c17344692b7301da953b1009ff3c856c7972f04
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2023-08-31 22:17:00 +0530

    Create a getters and setters for MedicalHistoryItem

--- app/src/main/java/com/example/capstoneprojectgroup4/MedicalHistoryItem.java
+++ app/src/main/java/com/example/capstoneprojectgroup4/MedicalHistoryItem.java
@@ -1,4 +1,28 @@
 package com.example.capstoneprojectgroup4;
 
-public class MedicalHistoryItem {
+public class MedicalHistoryItem
+{
+    // MedicalHistoryItem.java
+    public class MedicalHistoryItem
+    {
+        private String date;
+        private String description;
+
+        public MedicalHistoryItem(String date, String description)
+        {
+            this.date = date;
+            this.description = description;
+        }
+
+        public String getDate()
+        {
+            return date;
+        }
+
+        public String getDescription()
+        {
+            return description;
+        }
+    }
+
 }
