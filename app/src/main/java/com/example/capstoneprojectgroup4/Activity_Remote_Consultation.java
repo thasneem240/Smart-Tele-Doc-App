a7b47976cfee77923c452c237a58487bf6093af1
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2023-08-31 21:22:41 +0530

    Created an Empty Activity for Remote Consultation

---
+++ app/src/main/java/com/example/capstoneprojectgroup4/Activity_Remote_Consultation.java
@@ -0,0 +1,14 @@
+package com.example.capstoneprojectgroup4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Remote_Consultation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_consultation);
+    }
+}
