4ab7c93c11b512727c9052bac422b7a34bce969c
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2023-08-31 21:46:08 +0530

    Created a basic functionality for Remote Consultation setup

--- app/src/main/java/com/example/capstoneprojectgroup4/Activity_Remote_Consultation.java
+++ app/src/main/java/com/example/capstoneprojectgroup4/Activity_Remote_Consultation.java
@@ -3,12 +3,28 @@ package com.example.capstoneprojectgroup4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Activity_Remote_Consultation extends AppCompatActivity {
+
+public class Activity_Remote_Consultation extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_consultation);
+
+        Button videoConferenceButton = findViewById(R.id.videoConferenceButton);
+        videoConferenceButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Handle the video conference button click event here
+                // You can start the video conference activity or initiate the call.
+            }
+        });
     }
 }
