c71510cf8a75dadfad09f46df3be9f3deccd67ff
{DakshinaPerera} <{20537274@student.curtin.edu.au}>
2023-08-31 22:09:24 +0530

    Redo firebase logic to fix conflict with dialogflow

--- app/src/main/java/com/example/capstoneprojectgroup4/TransactionHistory.java
+++ app/src/main/java/com/example/capstoneprojectgroup4/TransactionHistory.java
@@ -4,7 +4,7 @@ package com.example.capstoneprojectgroup4;
 import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,9 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.example.capstoneprojectgroup4.authentication.PatientObject;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,10 +21,9 @@ import com.google.firebase.database.ValueEventListener;
 
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+
 
 public class TransactionHistory extends AppCompatActivity {
     ArrayList <TransactionHistoryData> data;
@@ -58,22 +55,16 @@ public class TransactionHistory extends AppCompatActivity {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     childrenList.add(snapshot);
                 }
-
-
                 for (int i = childrenList.size() - 1; i >= 0; i--) {
                     DataSnapshot Snapshot = childrenList.get(i);
                     data.add(Snapshot.getValue(TransactionHistoryData.class)) ;
                 }
                 adapter.notifyDataSetChanged();
-
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Log.w(TAG, "loadPost:onCancelled", error.toException());
             }
-
-
         });
     }
 
