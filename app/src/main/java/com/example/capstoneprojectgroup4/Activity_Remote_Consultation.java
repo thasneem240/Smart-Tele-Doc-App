e74fe4a6ecd2ddd2df94bd4805bdb8a88713cf9f
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2023-09-05 19:58:00 +0530

    Completed Video Conference feature and works perfectly

--- app/src/main/java/com/example/capstoneprojectgroup4/Activity_Remote_Consultation.java
+++ app/src/main/java/com/example/capstoneprojectgroup4/Activity_Remote_Consultation.java
@@ -2,13 +2,16 @@ package com.example.capstoneprojectgroup4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Activity_Remote_Consultation extends AppCompatActivity
 {
+    private EditText patientNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,6 +20,12 @@ public class Activity_Remote_Consultation extends AppCompatActivity
         setContentView(R.layout.activity_remote_consultation);
 
         Button videoConferenceButton = findViewById(R.id.videoConferenceButton);
+        patientNameEditText = findViewById(R.id.patientNameEditText);
+
+        patientNameEditText.setText("A.S.M. Thasneem");
+
+        Intent intent = new Intent(this, Activity_VideoConference.class);
+
         videoConferenceButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -24,6 +33,9 @@ public class Activity_Remote_Consultation extends AppCompatActivity
             {
                 // Handle the video conference button click event here
                 // You can start the video conference activity or initiate the call.
+                startActivity(intent);
+
+
             }
         });
     }
