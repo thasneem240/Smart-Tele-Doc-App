36dfe42f94fce4663016ddf5bdf2ec145a912cd7
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2023-09-21 22:52:11 +0530

    Created An Activity_NoteTaking to implement note taking feature in Video Conference

---
+++ app/src/main/java/com/example/capstoneprojectgroup4/Activity_NoteTaking.java
@@ -0,0 +1,15 @@
+package com.example.capstoneprojectgroup4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_NoteTaking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_taking);
+    }
+}
