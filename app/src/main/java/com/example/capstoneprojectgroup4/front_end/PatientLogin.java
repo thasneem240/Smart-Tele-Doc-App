4df13e2bad6d614819d7df2af862034c2f8fc5d1
{DakshinaPerera} <{20537274@student.curtin.edu.au}>
2023-09-21 23:53:55 +0530

    Made chages according to client feedback(In transaction)

--- app/src/main/java/com/example/capstoneprojectgroup4/front_end/PatientLogin.java
+++ app/src/main/java/com/example/capstoneprojectgroup4/front_end/PatientLogin.java
@@ -76,11 +76,11 @@ public class PatientLogin extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_patient_login, container, false);
-        Button login = v.findViewById(R.id.login_button);
-        TextView signup = v.findViewById(R.id.sign_up_link);
-        EditText email_ = v.findViewById(R.id.loginenter_email);
-        EditText password_ = v.findViewById(R.id.enter_password);
+        View v = inflater.inflate(R.layout.fragment_patient_login, container, false); //these lines give errors
+        Button login = v.findViewById(R.id.login_button); //these lines give errors
+        TextView signup = v.findViewById(R.id.sign_up_link); //these lines give errors
+        EditText email_ = v.findViewById(R.id.loginenter_email); //these lines give errors
+        EditText password_ = v.findViewById(R.id.enter_password); //these lines give errors
 
         auth = FirebaseAuth.getInstance();
         login.setOnClickListener(new View.OnClickListener() {
