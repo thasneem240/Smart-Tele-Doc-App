159645da2af1f369fc30cbf0128fd7f9a62d960d
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2023-09-26 15:59:33 +0530

    The uploading of lab reports to Firebase has been successfully completed.

--- app/src/main/java/com/example/capstoneprojectgroup4/Frag_LabReports.java
+++ app/src/main/java/com/example/capstoneprojectgroup4/Frag_LabReports.java
@@ -1,8 +1,10 @@
 package com.example.capstoneprojectgroup4;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,9 +19,24 @@ import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentManager;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.OpenableColumns;
+import android.widget.Toast;
 
 import com.example.capstoneprojectgroup4.front_end.MedicalRecords;
+import com.example.capstoneprojectgroup4.home.MainActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 /**
@@ -39,8 +56,13 @@ public class Frag_LabReports extends Fragment
     // Variable Declarations
     private static final int FILE_PICKER_REQUEST_CODE = 1;
     private Button selectFileButton;
-    private EditText selectedFileNameEditText;
+    private ImageView firebaseImage;
     private Button uploadButton;
+    private Uri imageUri;
+    private String fileName = null;
+
+    private StorageReference storageReference;
+    private ProgressDialog progressDialog;
 
 
     // Firebase
@@ -97,7 +119,8 @@ public class Frag_LabReports extends Fragment
 
 
         selectFileButton = view.findViewById(R.id.selectFileButton);
-        selectedFileNameEditText = view.findViewById(R.id.selectedFileNameEditText);
+        firebaseImage = view.findViewById(R.id.firebaseImage);
+
         uploadButton = view.findViewById(R.id.uploadButton);
 
         ImageView backButton = view.findViewById(R.id.backButtonLabReports);
@@ -118,7 +141,8 @@ public class Frag_LabReports extends Fragment
             @Override
             public void onClick(View v)
             {
-                openFilePicker();
+                //openFilePicker();
+                selectImage();
             }
         });
 
@@ -128,17 +152,19 @@ public class Frag_LabReports extends Fragment
             @Override
             public void onClick(View v)
             {
-                // Implement file upload logic here
-                // You can use the selected file (URI) for uploading to a server or saving locally.
-                String selectedFilePath = selectedFileNameEditText.getText().toString();
-                // Handle the upload logic accordingly.
+//                // Implement file upload logic here
+//                // You can use the selected file (URI) for uploading to a server or saving locally.
+//                String selectedFilePath = selectedFileNameEditText.getText().toString();
+//                // Handle the upload logic accordingly.
+
+                uploadFile();
+
             }
         });
 
 
         return  view;
     }
-
 
 
     private void openFilePicker()
@@ -148,20 +174,20 @@ public class Frag_LabReports extends Fragment
         startActivityForResult(intent, FILE_PICKER_REQUEST_CODE);
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == FILE_PICKER_REQUEST_CODE && resultCode == Activity.RESULT_OK)
-        {
-            if (data != null && data.getData() != null)
-            {
-                Uri selectedFileUri = data.getData();
-                selectedFileNameEditText.setText(selectedFileUri.toString());
-            }
-        }
-    }
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+//    {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == FILE_PICKER_REQUEST_CODE && resultCode == Activity.RESULT_OK)
+//        {
+//            if (data != null && data.getData() != null)
+//            {
+//                Uri selectedFileUri = data.getData();
+//                selectedFileNameEditText.setText(selectedFileUri.toString());
+//            }
+//        }
+//    }
 
     // Query the database for lab reports
   //  DatabaseReference labReportsRef = databaseReference.child("lab_reports").child(patientID);
@@ -213,6 +239,100 @@ public class Frag_LabReports extends Fragment
 
     // Call this method when you want to display patient records (e.g., in onCreateView)
     displayPatientRecords(patientID); // Pass the patient's unique ID as a parameter*/
+
+    private void selectImage()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,100);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100 && data != null && data.getData() != null)
+        {
+            imageUri = data.getData();
+            firebaseImage.setImageURI(imageUri);
+        }
+    }
+
+
+//    public static String getFileName(Context context, Uri uri)
+//    {
+//        String fName = null;
+//        ContentResolver contentResolver = context.getContentResolver();
+//
+//        // Use a Cursor to query information about the file
+//        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+//
+//        try
+//        {
+//            if (cursor != null && cursor.moveToFirst())
+//            {
+//                // Get the column index for the file name
+//                int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+//                fName = cursor.getString(nameIndex);
+//            }
+//        } finally
+//        {
+//            if (cursor != null)
+//            {
+//                cursor.close();
+//            }
+//        }
+//
+//        return fName;
+//    }
+
+    private void uploadFile()
+    {
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setTitle("Uploading File.....");
+        progressDialog.show();
+
+
+        //String Uid = MainActivity.getPatientObject().getUid();
+
+        SimpleDateFormat formattter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.ENGLISH);
+        Date now = new Date();
+        String fileTitle = formattter.format(now);
+
+        storageReference = FirebaseStorage.getInstance().getReference("Lab_Reports/" + fileTitle);
+        storageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>()
+                {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+                    {
+                        firebaseImage.setImageURI(null);
+                        Toast.makeText(getActivity(),"Successfully uploaded", Toast.LENGTH_SHORT).show();
+
+                        if(progressDialog.isShowing())
+                        {
+                            progressDialog.dismiss();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener()
+                {
+                    @Override
+                    public void onFailure(@NonNull Exception e)
+                    {
+                        if(progressDialog.isShowing())
+                        {
+                            progressDialog.dismiss();
+                        }
+                        Toast.makeText(getActivity(),"Failed to upload", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
+
+
 
 
 
