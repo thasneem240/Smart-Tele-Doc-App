26a2b3ddd9b78b315af0842feabff10c6c9bbdc1
{DakshinaPerera} <{20537274@student.curtin.edu.au}>
2023-10-06 10:20:56 +0530

    Changed AI feature interface

--- app/src/main/java/com/example/capstoneprojectgroup4/transaction/TransactionHistoryData.java
+++ app/src/main/java/com/example/capstoneprojectgroup4/transaction/TransactionHistoryData.java
@@ -1,13 +1,22 @@
 package com.example.capstoneprojectgroup4.transaction;
 
+import com.google.type.DateTime;
+
+import java.text.DateFormat;
+
 public class TransactionHistoryData {
     private String name;
+    private DateFormat date;
+    private double price;
+
+
 
     public TransactionHistoryData(){};
 
 
     public TransactionHistoryData(String name) {
         this.name = name;
+
 
     }
 
@@ -21,4 +30,11 @@ public class TransactionHistoryData {
     public void setName(String name) {
         this.name = name;
     }
+    public DateFormat getDate() { return date; }
+
+    public void setDate(DateFormat date) {this.date = date;}
+
+    public double getPrice() {return price;}
+
+    public void setPrice(double price) {this.price = price;}
 }
